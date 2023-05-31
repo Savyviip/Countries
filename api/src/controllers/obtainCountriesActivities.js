e9d8ff36a383818fe78c763a04264d3cbd9d03b4
savyviip <savy.viip@gmail.com>
2023-05-31 02:21:22 -0300

    Country Project

--- api/src/controllers/obtainCountriesActivities.js
+++
@@ -1,8 +0,0 @@
-const {Activity, Country} = require('../db');
-
-const obtainCountriesActivities = async(id) => await Country.findAll({where: {id: id}, 
-    include: [{model: Activity, attributes: ['name', 'difficulty', 'duration', 'season'],
-    through: { attributes: [] }
-}]});
-
-module.exports = obtainCountriesActivities;
